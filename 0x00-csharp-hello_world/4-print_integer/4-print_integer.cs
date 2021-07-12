808f62431ec4e53ce6b5d834721ed75821a6e597
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-12 22:40:02 +0000

    How to print a variable

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace _4_print_integer
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int number = 972;
-		    Console.WriteLine("{0} {1}", number, "Mission Street");
-        }
+        int number = 972;
+        Console.WriteLine("{0} {1}", number, "Mission Street");
     }
 }
