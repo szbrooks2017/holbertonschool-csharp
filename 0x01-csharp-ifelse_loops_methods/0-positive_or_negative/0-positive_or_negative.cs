494d465868eccd6fe0e222fa56072f99159df2cb
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-13 17:16:13 +0000

    How to use an if/else statement

--- 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -6,19 +6,18 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
-        if (number == 0)
-        {
-            Console.WriteLine("{0} is zero", number);
-        }
         
         if (number < 0)
         {
             Console.WriteLine("{0} is negative", number);
         }
+        else if (number == 0)
+        {
+            Console.WriteLine("{0} is zero", number);
+        }
         else
         {
             Console.WriteLine("{0} is positive", number);
         }
-
     }
 }
