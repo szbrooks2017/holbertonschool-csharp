44f28ca0239fc560dbd8b30f49b0fd68aeb281a2
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-13 22:15:49 +0000

    How to index through a range to find select data

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+        {
+            if (i == 99)
+            {
+                Console.Write("{0}\n", i);
+            }
+            else
+            {
+                Console.Write("{0}, ", i);   
+            }
+        }
+    }
+}
