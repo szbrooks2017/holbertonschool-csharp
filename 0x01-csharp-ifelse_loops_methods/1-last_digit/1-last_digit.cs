dea69fdebc1756d3b81b0ecbaa302b1081aadff3
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-13 18:16:07 +0000

    How to find the last digit

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -9,7 +9,7 @@ class Program
         int lastDigit = number % 10;
         string expression;
 
-        if (lastDigit < 5)
+        if (lastDigit > 5)
         {
             expression = " and is greater than 5";
         }
@@ -20,6 +20,7 @@ class Program
         else
         {
             expression = " and is less than 6 and not 0";
+
         }
         Console.WriteLine("The last digit of {0} is {1}{2}", number, lastDigit, expression);
     }
