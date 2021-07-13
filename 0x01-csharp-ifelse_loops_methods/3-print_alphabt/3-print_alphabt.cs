a6c805585ca857e9569ab09da27e31502c11bc18
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-13 18:54:48 +0000

    How to cut out letters of the alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++)
+            if (letter != 'q' && letter != 'e')
+                Console.Write(letter);
+    }
+}
