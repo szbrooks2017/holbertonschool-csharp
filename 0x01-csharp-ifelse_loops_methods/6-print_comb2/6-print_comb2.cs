4db98128d1e130f9e60457834cccfea777e4a48a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-13 23:08:02 +0000

    How to index an index uniquely

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            for (int j = 0; j <= 9; j++)
+            {
+                if (i != j && i < 9 && i < j)
+                {
+                    if (i == 8 && j == 9)
+                    {
+                        Console.WriteLine("{0}{1}", i, j);
+                    }
+                    else
+                    {
+                        Console.Write("{0}{1}, ", i, j);
+                    }
+                }
+            }
+        }
+    }
+}
