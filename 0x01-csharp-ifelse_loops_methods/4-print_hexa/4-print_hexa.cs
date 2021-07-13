fb166e2214d071988ca2dabb556ec076cffb6135
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-13 22:01:39 +0000

    How to convert int to hex

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 99; i++)
+        {
+            string hex = i.ToString("X");
+            Console.WriteLine("{0} = 0x{1}", i, hex);
+        }
+    }
+}
