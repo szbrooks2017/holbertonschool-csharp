fd22e75c368fdf4f50dc0b061f67d480b2be2c9c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-14 00:55:44 +0000

    FizzBuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Fizzbuzz
+{
+    static void Main(string[] args)
+    {
+        for (int num = 0; num < 101; num++)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.Write("FizzBuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.Write("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.Write("Buzz");
+            }
+            else
+            {
+                Console.Write(num);
+            }
+        }
+    }
+}
