0c6fa6e760ad4e550da5245dccbd657556095370
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-14 19:00:33 +0000

    How to replace an element at a given index

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// return updated array
+// replace an element at a given index
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            array[index] = n;
+        }
+        else
+        {
+            Console.WriteLine("Index out of range");
+        }
+        return array;
+    }
+}
