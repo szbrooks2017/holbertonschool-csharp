c26fe2f7d529ddf8fad7446804d2cd5923eba5c1
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-14 23:32:44 +0000

    How to print a list of ints

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+// create and print list of ints
+// the integers in the list start with 0 and increment
+// return new list
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+        }
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int>newList = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            newList.Add(i);
+            Console.Write(i);
+            // if (i == size - 1)
+        }
+        return newList;
+    }
+}
+
