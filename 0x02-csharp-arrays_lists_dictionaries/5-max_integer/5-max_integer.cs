251d8e0790081ee0e1e08f78e882c922cfb90a5d
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-14 23:53:58 +0000

    How to create custom Max()

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -8,7 +8,7 @@ class List
 {
     public static int MaxInteger(List<int> myList)
     {
-        if (myList == null)
+        if (myList.Count == 0)
         {
             Console.WriteLine("List is empty");
             return -1;
