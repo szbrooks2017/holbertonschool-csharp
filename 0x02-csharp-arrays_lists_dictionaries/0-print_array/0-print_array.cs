3f2c37ee17f11d7cd8afaee3990ee8d725a0cf47
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-14 17:31:20 +0000

    How to create/print an Array of ints

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        // int should start with 0, increment by one
+        // if size is 0 print blank line
+        // if size < 0 print size cannot be negative return null
+        int[] newArray = new int[size];
+        
+        if (size == 0)
+        {
+            Console.WriteLine("");
+        }
+
+        if (size <= 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+           if (newArray[i] < size - 1)
+           {
+               Console.Write(i + ", ");
+           } 
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+        return newArray;
+    }
+}
+
