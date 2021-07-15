7c69e60cff9f924a706ef56d07eb2ff1c50a8024
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 22:56:12 +0000

    A full introduction to queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+// create a Queue
+// print number of items in aQueue with proper format
+// print items on top of aQueue without removing with proper format
+// print aQueue if it contains a given item search
+// if aQueue contains the givien item search, remove all items up to including search
+// add newItem to aQueue
+// return aQueue
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        // count of items in Queue
+        int count = aQueue.Count;
+        Console.WriteLine("Number of items: {0}", count);
+
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            // top item
+            string topItem = aQueue.Peek();
+            Console.WriteLine("Top item: {0}", topItem);
+        }
+
+        // if it contains search
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+
+        while (aQueue.Contains(search) == true)
+        {
+            aQueue.Dequeue();
+        }
+
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
