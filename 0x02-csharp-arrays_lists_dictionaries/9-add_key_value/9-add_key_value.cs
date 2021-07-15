9dcdc90da6178a7c0ef0e88ba2356a39b7527b76
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 01:21:42 +0000

    How to add key/value to dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+// add a key and value to a dict
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)        {
+        myDict[key] = value;
+        return myDict;
+    }
+}
