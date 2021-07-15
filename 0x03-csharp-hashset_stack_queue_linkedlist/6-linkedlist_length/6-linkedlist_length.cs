6f82aeb4220735fe9d4c293d093985d3540689bf
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 23:35:39 +0000

    How to create custom Linked List Count()

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// return the number of elements in a Linked List
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        foreach(int item in myLList)
+        {
+            count++;
+        }
+        return count;
+    }
+}
