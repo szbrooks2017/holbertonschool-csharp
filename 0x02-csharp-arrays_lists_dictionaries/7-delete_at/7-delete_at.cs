c6ed95d884f3ec4d0a1170903ba4e8206f6eae88
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 00:36:17 +0000

    How to create a custom RemoveAt()

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// delete item at location.
+// return updated list
+// account for index out of range and return unchanged list
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index >= 0 && index < myList.Count)
+        {
+            myList.Remove(index);
+        }
+        else
+        {
+            Console.WriteLine("Index is out of range");
+        }
+        return myList;
+    }
+}
+
