f5273f6bda5026060432ea02775643dab5643172
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 03:29:09 +0000

    How to square a matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int rows = myMatrix.GetLength(0);
+        int columns = myMatrix.GetLength(1);
+        for(int x = 0; x < rows; x++)
+        {
+            for(int y = 0; y < columns; y++)
+            {
+                myMatrix[x, y] *= myMatrix[x, y];
+            }
+        }
+        return myMatrix;
+    }
+}
