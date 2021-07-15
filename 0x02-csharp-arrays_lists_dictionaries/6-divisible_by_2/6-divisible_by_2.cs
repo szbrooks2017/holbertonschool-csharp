5eaae46ab5362c3cf4ecb5d3b01020022b17c274
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 00:07:56 +0000

    How to find multiples of 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// finds all multiples of 2 in a list
+// returns a new list true or false, 
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool>tfList = new List<bool>();
+        for (int i = 1; i <= myList.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                tfList.Add(true);
+            }
+            else
+            {
+                tfList.Add(false);
+            }
+        }
+        return tfList;
+    }
+}
