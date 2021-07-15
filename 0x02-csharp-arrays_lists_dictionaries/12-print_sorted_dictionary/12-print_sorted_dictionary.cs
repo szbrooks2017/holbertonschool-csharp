e2eb664e9be99d276ddb17a3b54f7ef7320a0890
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 02:11:31 +0000

    How to sort a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var temp = myDict.Keys.ToList();
+        temp.Sort();
+
+        foreach (var k in temp)
+        {
+            Console.WriteLine("{0}: {1}", k, myDict[k]);
+        }
+    }
+}
