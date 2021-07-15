ec4d409471bd68f2b18eda8a3405686a9ab62c98
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 23:44:48 +0000

    How to add a new node to the beginning

--- 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -6,7 +6,7 @@ class LList
 {
     public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
     {
-        LinkedListNode<int> nnode = myLList.Addfirst(n);
+        LinkedListNode<int> nnode = myLList.AddFirst(n);
         return nnode;
     }
 }
