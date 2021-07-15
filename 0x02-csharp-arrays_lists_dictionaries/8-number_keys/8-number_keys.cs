b7d9c04498ae98eb32f0118f924fa91b8bea09ef
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 00:55:20 +0000

    How to create a custom dict.count()

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// return number of keys in a dictionary
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+        foreach (string key in myDict.Keys)
+        {
+            if (key != null)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
