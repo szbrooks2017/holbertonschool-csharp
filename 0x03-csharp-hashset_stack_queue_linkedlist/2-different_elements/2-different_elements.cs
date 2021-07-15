f66721f3a543b207415a2cefbb8c4e66eedbb800
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 21:01:51 +0000

    Using HashSets with SymmetricExceptWith

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
+// Returns a new sorted list of all elements present in one or the other list but not both of the given lists
 class List
 {
     public static List<int> DifferentElements(List<int> list1, List<int> list2)
@@ -10,6 +10,7 @@ class List
         HashSet<int> second = new HashSet<int>(list2);
         first.SymmetricExceptWith(second);
         List<int> sorted = new List<int>(first);
+        sorted.Sort();
         return sorted;
     }
 }
