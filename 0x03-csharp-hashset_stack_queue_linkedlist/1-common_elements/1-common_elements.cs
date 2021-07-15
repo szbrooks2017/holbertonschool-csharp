3c57e595986df06be956c6f071235caf76ad0008
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 20:47:12 +0000

    How to create custom list sort

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+// return a sorted list of common elements in two lists. 
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> first = new HashSet<int>(list1);
+        HashSet<int> second = new HashSet<int>(list2);
+        first.IntersectWith(second);
+        List<int> sorted = new List<int>(first);
+        return sorted;
+    }
+}
