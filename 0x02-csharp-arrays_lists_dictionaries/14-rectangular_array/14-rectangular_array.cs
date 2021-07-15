560ab02e9b3c80b02b5f1d2e3e364a26e41374e1
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 02:58:50 +0000

    How to create a grid

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int width = 5;
+        int height = 5;
+
+        int[,] grid = new int[width,height];
+        grid[2,2] = 1;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Console.Write(grid[x,y] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
