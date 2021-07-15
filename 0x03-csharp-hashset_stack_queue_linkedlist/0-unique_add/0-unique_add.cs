58f45f4ae1b3dad1a3b1fe3554f88d972262709f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 20:05:14 +0000

    How to use HashSets to find sum

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+// write a method that adds all unique ints in a list
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> sumNumb = new HashSet<int>(myList);
+
+        int total = 0;
+        foreach (int i in sumNumb)
+        {
+            total += i;
+        }
+        return total;
+    }
+}
