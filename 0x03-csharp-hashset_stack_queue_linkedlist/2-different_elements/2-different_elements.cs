70e2a4c452bb0e0dfc6148f923a165d52417152e
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 20:59:58 +0000

    Using HashSets with SymmetricExceptWith

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> first = new HashSet<int>(list1);
+        HashSet<int> second = new HashSet<int>(list2);
+        first.SymmetricExceptWith(second);
+        List<int> sorted = new List<int>(first);
+        return sorted;
+    }
+}
+
