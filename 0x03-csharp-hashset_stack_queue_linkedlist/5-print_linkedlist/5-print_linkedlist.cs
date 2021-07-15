4188e7faff5759abe9c776e2c68b24b2ec8e4862
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-15 23:26:35 +0000

    How to create and print a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+// create and print a linked list of integers.
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+
+        if (size == 0)
+        {
+            return newList;
+        }
+
+        int i = 0;
+
+        while (i < size)
+        {
+            newList.AddFirst(i);
+            System.Console.WriteLine(i);
+            i++;
+        }
+        return newList;
+    }
+}
