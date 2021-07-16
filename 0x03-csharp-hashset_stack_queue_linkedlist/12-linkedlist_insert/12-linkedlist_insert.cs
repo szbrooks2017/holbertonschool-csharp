45ee3bd06b8a4ccfd068ee93e84f7552eacb09ff
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-16 03:06:45 +0000

    adding for untracked

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+// insert new node in the correct position.
+// get the head?
+// when head == nth insert before?
+
+class LList
+
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> head = myLList.First;
+        LinkedListNode<int> nnode = new LinkedListNode<int>(n);
+
+        if (head == null)
+        {
+            myLList.AddFirst(nnode);
+            return nnode;
+        }
+
+        if (head.Value > n)
+        {
+            myLList.AddBefore(head, nnode);
+            return nnode;
+        }
+    }
+}
