3c889eda963b58de7786076da8c4e9f5cbf01a51
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-16 00:02:17 +0000

    How to find the index of a Linked List

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+// return index of occurrence or -1
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = myLList.TakeWhile(n => n != value).Count();
+        return i;
+    }
+}
