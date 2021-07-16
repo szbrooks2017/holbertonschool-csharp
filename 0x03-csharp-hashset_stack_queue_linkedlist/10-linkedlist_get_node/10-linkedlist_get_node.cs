d49d81f656873994a95002714cc8500f4059ef12
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-16 01:19:59 +0000

    How to delete a node in a Linked List

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+// return the vaule of the nth node
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+
+        var value = myLList.ElementAt(n);
+        if (value != n)
+        {
+            return value;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
