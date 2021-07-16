1875ab8a5a96a5decd3fd306a146cfe0ff3d3ce2
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-16 01:47:06 +0000

    How to add sum of Linked List

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        return myLList.Sum();
+    }
+}
