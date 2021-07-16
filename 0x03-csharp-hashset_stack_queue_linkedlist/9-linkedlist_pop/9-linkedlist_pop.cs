91281f260b0f16546fdeaae45fb9fca28616d223
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-16 00:42:21 +0000

    How to delete a node in a Linked List

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int head = myLList.First.Value;
+        myLList.RemoveFirst();
+
+        return head;
+
+    }
+}
