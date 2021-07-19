54fb06579d8aad19ef73e7b653265702b9a7cfe0
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-19 18:31:48 +0000

    How to handle more than one exception

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> newList = new List<int>();
+        int c = 0;
+        for (int i = 0; i < listLength; i++)
+        {
+            c = 0;
+            try
+            {
+                c = list1[i] / list2[i];
+                newList.Add(c);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                newList.Add(c);
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return newList;
+    }
+}
