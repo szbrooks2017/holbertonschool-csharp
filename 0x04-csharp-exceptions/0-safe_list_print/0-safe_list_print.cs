b966ffe8216f5045629e7dda6a1aa852c0d807b6
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-19 17:35:11 +0000

    How to use try/catch with exception

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        
+        try
+        {
+            while (i < n)
+            {
+                Console.WriteLine(myList[n]);
+                i++;
+            }
+        }
+        catch (Exception)
+        {
+            return i;
+        }
+    return i;
+    }
+}
