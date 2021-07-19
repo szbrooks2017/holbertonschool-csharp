52336751cc890c6615230daf23fdd712a4c9d988
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-19 18:03:52 +0000

    How to use specific exception with finally block

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int c = 0;
+        try
+        {
+            c = a / b;
+        }
+        catch(DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, c);
+        }
+    }
+}
