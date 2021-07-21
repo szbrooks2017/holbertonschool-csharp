c24793984688c9ad12002562a195d4ffd375cb1b
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-21 22:50:46 +0000

    Adding a public constructor with an exception

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Rerpresents Zombie class</summary>
+    class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
