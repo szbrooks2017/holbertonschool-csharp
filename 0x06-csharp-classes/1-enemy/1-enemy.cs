6eb30d6e5a7d82b9cbbbace55f89435778426c0a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-21 22:45:17 +0000

    How to add public field, public constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Rerpresents Zombie class</summary>
+    class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
