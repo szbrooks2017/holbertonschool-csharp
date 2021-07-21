4999eb19613b905e50d971edbbcc8b37ffbbefdb
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-21 23:07:36 +0000

    Adding a private field, introducing get/set

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Rerpresents Zombie class</summary>
+    class Zombie
+    {
+        public int health;
+        private string name = "(No name)";
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+
+        public string Name
+        {
+            get {return (name);}
+            set {name = value;}
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
