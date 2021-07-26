f7a8bc89174f5c136a7bd0a10e9b4f6933a12a5e
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-26 16:41:19 +0000

    Fixing variable error

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 namespace MyMath.Tests
 {
     [TextFixture]
-    public class Tests
+    public class OperationsTests
     {
         [Test]
         public void Add_postive()
@@ -11,7 +11,7 @@ namespace MyMath.Tests
             int a = 98;
             int b = 2;
             int result = MyMath.Operations.Add(a,b);
-            Assert.AreEqual(a + b, c);
+            Assert.AreEqual(a + b, result);
         }
     }
 }
