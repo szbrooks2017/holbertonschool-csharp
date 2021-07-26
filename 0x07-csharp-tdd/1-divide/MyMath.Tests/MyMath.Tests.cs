b1360bef9f17af0197854dafbe9406b2a3373cda
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-26 19:04:07 +0000

    Adding tests for task 1

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -15,5 +15,13 @@ namespace MyMath.Tests
             int[,] result = Matrix.Divide(matrix, n);
             Assert.AreEqual(null, result);
         }
+        [Test]
+        public void Test2()
+        {
+            int[,] matrix = new int[,] {{6, 10}, {30, 40}, {4, 8}};
+            int n = 2;
+            int[,] result = Matrix.Divide(matrix, n);
+            Assert.AreEqual(new int[,] {{3, 5}, {15, 20}, {2, 4}}, result);
+        }
     }
 }
