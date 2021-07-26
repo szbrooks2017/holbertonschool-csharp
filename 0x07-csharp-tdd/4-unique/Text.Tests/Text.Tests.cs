6f8ffe915e66498cc8a952e47484bf54aae85e6a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-26 23:34:30 +0000

    adding first test

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -2,13 +2,17 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
+    [TestFixture]
+    ///<summary> test for unique</summary>
     public class Tests
     {
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            string s = "";
+            var result = Str.UniqueChar(s);
+            Assert.AreEqual(-1, result);
         }
     }
 }
