e21fb0f7c38e8ce8e3002dc7bd0280068e294906
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-26 23:47:47 +0000

    editing formatting

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -8,35 +8,35 @@ namespace Text.Tests
     {
 
         [Test]
-        public void Test1()
+        public void Test_no_string()
         {
             string s = "";
             var result = Str.UniqueChar(s);
             Assert.AreEqual(-1, result);
         }
         [Test]
-        public void Test2()
+        public void Test_middle_index()
         {
             string s = "helloholberton";
             var result = Str.UniqueChar(s);
             Assert.AreEqual(8, result);
         }
         [Test]
-        public void Test3()
+        public void Test_last_index()
         {
             string s = "hhht";
             var result = Str.UniqueChar(s);
             Assert.AreEqual(3, result);
         }
         [Test]
-        public void Test4()
+        public void Test_first_index()
         {
             string s = "httt";
             var result = Str.UniqueChar(s);
             Assert.AreEqual(0, result);
         }
-                [Test]
-        public void Test5()
+        [Test]
+        public void Test_no_unique()
         {
             string s = "hhhhh";
             var result = Str.UniqueChar(s);
