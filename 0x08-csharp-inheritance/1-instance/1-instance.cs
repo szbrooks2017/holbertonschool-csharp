8206ced8f29c435c7a82b7dcefdb16c82e4deec7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-27 21:23:31 +0000

    Using 'is' again

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary> return true if the obj is an instance of or if the obj</summary>
+///<summary> is an instance from array</summary>
+class Obj
+{
+    ///<summary> method that returns true if an instance of inherited class else false</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (obj is Array);
+    }
+}
