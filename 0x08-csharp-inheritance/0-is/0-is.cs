db110b66ef27e84d73104fbc98ce0d986e41fcc0
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-27 21:12:17 +0000

    How to use 'is'

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+///<summary> wite a method that returns true if the obj is an int</summary>
+class Obj
+{
+    ///<summary> true if obj is an int, else false</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if(obj is int)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
