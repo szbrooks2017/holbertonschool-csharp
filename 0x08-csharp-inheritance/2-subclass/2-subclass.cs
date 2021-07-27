e9c5b357cf37a60cc58021feca1219131ea13324
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-27 21:38:46 +0000

    How to find the subclass of a base class

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+///<summary> return true if the obj is an instance of a class that inherits, else false</summary>
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
