618b5b0ad93e14d56bafcf98ab75a62580777c3c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 01:14:16 +0000

    Creating a base class Shape

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Shape
+{
+    ///<summary> the area of a shape</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+
+}
