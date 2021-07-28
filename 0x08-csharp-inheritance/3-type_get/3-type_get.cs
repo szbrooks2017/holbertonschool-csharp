856ac6b24bca9ab2b7a4e7235a500ffee07320e3
szbrooks2017 <strattonbrooks13@gmail.com>
2021-07-28 00:02:39 +0000

    How to use PropertyInfo and MethodInfo

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+///<summary> a method that prints the names of the available properties and methods of an object.</summary>
+class Obj
+{
+    ///<summary> prints the available properties</summary>
+    public static void Print(object myObj)
+    {
+        // for properties
+        Type t = myObj.GetType();
+        PropertyInfo[] pi = t.GetProperties();
+        Console.WriteLine(myObj.GetType().Name + "Properties:");
+        foreach (PropertyInfo p in pi)
+        {
+            Console.WriteLine(p.Name);
+        }
+
+        // for methods
+        Type tt = myObj.GetType();
+        MethodInfo[] mi = tt.GetMethods();
+        Console.WriteLine(myObj.GetType().Name + "Methods:");
+        foreach (MethodInfo m in mi)
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
