c548042861799730448f5e39781d5d440948bb32
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 05:27:14 +0000

    Method that calculates the dot product of a vector

---
+++ 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+///<summary>Class for vectors.</summary>
+class VectorMath
+{
+    ///<summary>Method that returns product of 2d/3d vectors.</summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double result = -1;
+        if (vector1.Length == 2 || vector1.Length == 3 && vector1.Length == vector2.Length) 
+        {
+            result = 0;
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                result += vector1[i] * vector2[i];
+            }
+            return result;
+        }
+    }
+}
