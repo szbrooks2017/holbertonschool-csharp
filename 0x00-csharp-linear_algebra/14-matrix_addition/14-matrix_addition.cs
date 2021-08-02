792edda6991018196dfad037e8fdcf6eedea0114
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 16:09:42 +0000

    Method that returns the sum of a matrix

---
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+///<summary>Class for matrices.</summary>
+class MatrixMath
+{
+    ///<summary>Returns the sum of two matrices.</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] matrix3;
+
+        if (matrix1.GetLength(0) != matrix2.GetLength(1))
+        {
+            return -1;
+        }
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return matrix3;
+    }
+}
