ad0c12701bb868899d5851bc6bf9805324e58a6b
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 17:54:14 +0000

    Multiplying a matrix by a scalar

---
+++ 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] matrix3;
+        double[,] bad = new double[,] {{-1}};
+        int row = matrix1.GetLength(0);
+        int column = matrix1.GetLength(1);
+        int row2 = matrix2.GetLength(0);
+
+        if (row == 2 && column == 2)
+        {
+            matrix3 = new double[2, 2];
+        }
+        else if (row == 3 && column == 3)
+        {
+            matrix3 = new double[3, 3];
+        }
+        else
+        {
+            return bad;
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            matrix3[i] = matrix[i] * scalar;
+        }
+        return matrix3;
+    }
+}
