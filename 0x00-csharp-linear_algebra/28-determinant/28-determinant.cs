e4ce3d136d403ab9d97b102ad2f5a6bd7db14787
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 23:33:25 +0000

    Method to find the determinant

---
+++ 0x00-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+///<summary> Class for matrices</summary>
+class MatrixMath
+{
+    ///<summary>Determines the determinant of a matrix</summary>
+    public static double Determinant(double[,] matrix)
+    {
+        int det = 0;
+
+        for(int i = 0; i < 3; i++)
+        {
+            det += (matrix[0, i] * (matrix[1, (i + 1) % 3] * matrix[2, (i + 2) % 3] - matrix[1, (i + 2) % 3] * matrix[2, (i + 1) % 3]));
+        }
+        return det;
+    }
+}
