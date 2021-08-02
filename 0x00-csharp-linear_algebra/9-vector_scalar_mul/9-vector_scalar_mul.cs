f00ac3e8dd30474d1758386671445f25ee5fe0c2
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 04:50:20 +0000

    Method that multiplies a vector by a scalar

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+///<summary>Class for Vectors.</summary>
+class VectorMath
+{
+    ///<summary>Multiplies a vector by a scalar.</summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double result = new double[1] {-1};
+
+        if (vector.Length == 2 || vector.Length == 3)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                result[i] = vector[i] * scalar;
+            }
+        }
+        return result;
+    }
+}
