230361dd1282f264d0ba7bea6a7b587b44e5ba95
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 02:54:47 +0000

    A method that returns the length of a vector

--- 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
-///<summary> a class for vectors</summary>
+///<summary>A class for vectors.</summary>
 class VectorMath
 {
-    ///<summary> a method that returns the length of a 2d or 3d vector</summary>
+    ///<summary>A method that returns the length of a 2d or 3d vector.</summary>
     public static double Magnitude(double[] vector)
     {
         if (vector.Length == 2)
@@ -14,7 +14,7 @@ class VectorMath
         }
         else if (vector.Length == 3)
         {
-            double result = (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2))));
+            double result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
             return (Math.Round(result, 2));
         }
         else
