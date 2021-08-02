a7900c586d250a6958b9951c1372916df712d0d9
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 23:53:36 +0000

    Method that returns a determinant

--- 0x00-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x00-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -6,12 +6,24 @@ class MatrixMath
     ///<summary>Determines the determinant of a matrix</summary>
     public static double Determinant(double[,] matrix)
     {
-        int det = 0;
+        double det = 0;
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
 
-        for(int i = 0; i < 3; i++)
+        if (row == 2 && row == column)
         {
-            det += (matrix[0, i] * (matrix[1, (i + 1) % 3] * matrix[2, (i + 2) % 3] - matrix[1, (i + 2) % 3] * matrix[2, (i + 1) % 3]));
+            return Math.Round((matrix[0,0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0]), 2);
         }
-        return det;
+        else if (row ==3 && row == column)
+        {
+            double a = matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]));
+            double b = matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]));
+            double c = matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]));
+            return Math.Round((a - b + c), 2);
+        }
+        else
+        {
+            return -1;
+        }
     }
 }
