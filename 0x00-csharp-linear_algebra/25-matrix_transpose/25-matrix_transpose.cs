b74ef2768903c98e86851c9360354dd33e337519
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 22:47:04 +0000

    Method that transposes a matrix

---
+++ 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+///<summary> Class for matrices</summary>
+class MatrixMath
+{
+    ///<summary> Transpose a matrix</summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
+        double[,] matrix3 = new double[row, column];
+
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < column; j++)
+            {
+                matrix3[j, i] = matrix[i, j];
+            }
+        }
+        return matrix3;
+    }
+}
