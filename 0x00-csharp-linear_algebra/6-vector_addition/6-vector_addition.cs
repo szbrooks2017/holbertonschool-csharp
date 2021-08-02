c48ca232546ce72c9e619f3c7b07e5f1a0d917a7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-02 03:57:39 +0000

    Method that returns sum of two vectors

--- 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -8,9 +8,9 @@ class VectorMath
     {
         double[] newVector = new double[1] {-1};
 
-        if (vector1.Length == 2 || vector2.Length == 2)
+        if (vector1.Length == 2 && vector2.Length == 2)
             newVector = new double[2] {(vector1[0] + vector2[0]), (vector1[1] + vector2[1])};
-        else if (vector1.Length == 3 || vector2.Length == 3)
+        else if (vector1.Length == 3 && vector2.Length == 3)
             newVector = new double[3] {(vector1[0] + vector2[0]), (vector1[1] + vector2[1]), (vector1[2] + vector2[2])};
         return newVector;
     }
