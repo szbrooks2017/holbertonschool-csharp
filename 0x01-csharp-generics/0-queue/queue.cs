db65401f0724e9e4dc6926305f394a043cd85adf
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-03 21:57:32 +0000

    Creating custom queue<t> class

---
+++ 0x01-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Custome Queue class</summary>
+class Queue<T>
+{
+    ///<summary> Method that returns the type</summary>
+    public T CheckType()
+    {
+        return typeof(T);
+    }
+}
