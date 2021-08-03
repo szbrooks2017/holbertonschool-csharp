74f851b364306c12e8e183704a540ae88775481f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-03 00:51:27 +0000

    Method that returns the crossproduct

--- 0x00-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x00-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -1,25 +1,23 @@
 ﻿using System;
 
-///<summary> Class for matrices</summary>
-class MatrixMath
+///<summary> Class for Vector</summary>
+class VectorMath
 {
     ///<summary>Determines the cross product of two vectors</summary>
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        int row = matrix.GetLength(0);
-        int column = matrix.GetLength(1);
+        int row = vector1.GetLength(0);
+        int column = vector2.GetLength(0);
         double[] bad = new double[] {-1};
         double[] cross = new double[3];
 
-        if (row != 3 && row != column)
+        if (row != 3 && column != 3)
         {
             return bad;
         }
-
-        cross[0] = (vector1[1] * vector2[2]) - (vector2[2] * vector2[1]);
+        cross[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
         cross[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
         cross[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
         return cross;
-        }
     }
 }
