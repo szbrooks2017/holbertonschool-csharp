ff562f08142370c83c7512197291bd4b39dbf20c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-03 02:07:09 +0000

    Method for an inverse of a 2x2 matrix

---
+++ 0x00-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        int row = matrix.GetLength(0);
+        int column = matrix.GetLength(1);
+        double[,] bad = new double[,] {{-1}};
+        double[,] inverse = new double[,]
+        double det = MatrixMath.Determinant(matrix);
+
+        if (row != 2 && column != 2)
+        {
+            return bad;
+        }
+        inverse = {{(1 / det) * matrix[1, 1], (1 / det) * -matrix[0, 1]}, {(1 / det) * -matrix[1, 0], (1 / det) * matrix[0, 0]}};
+        return inverse;
+    }
+}
