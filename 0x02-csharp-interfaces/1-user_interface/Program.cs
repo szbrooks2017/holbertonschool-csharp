b3c6c08a5fc289c0f3806e4555794f49697d5147
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-04 19:04:09 +0000

    How to create and inherit multiple interfaces

---
+++ 0x02-csharp-interfaces/1-user_interface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+///<summary> Base class</summary>
+abstract class Base
+{
+    ///<summary> public property</summary>
+    public string name = "";
+
+    ///<summary>method that overrides ToString</summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType()}";
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Collect() {}
+    public void Break() {}
+    public void Interact() {}
+}
