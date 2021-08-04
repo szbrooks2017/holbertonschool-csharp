e33a00ed14dd62d2f1ec2bf7daf43fd7a78dcbfd
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-04 18:29:49 +0000

    Create an abstract class for interface

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary> Base class</summary>
+abstract class Base
+{
+    ///<summary> public property</summary>
+    public string name;
+
+    ///<summary>method that overrides ToString</summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType()}";
+    }
+
+}
