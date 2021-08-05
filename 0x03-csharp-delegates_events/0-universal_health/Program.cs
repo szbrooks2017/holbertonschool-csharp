82947476f01b1332645e7fbe0a87544bdeaa7c30
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-05 20:40:04 +0000

    Create a public class Player

--- 0x03-csharp-delegates_events/0-universal_health/Program.cs
+++ 0x03-csharp-delegates_events/0-universal_health/Program.cs
@@ -5,30 +5,27 @@ class Player
 {
     // shouldn't be able to access player's properties
     private string name { get; set; }
-    private float maxHP { get; set; }
+    private float maxHp { get; set; }
     private float hp { get; set; }
 
     ///<summary> constructor for the player stats</summary>
-    public Player(string name = "Player", float maxHP = 100f)
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        if (maxHP < 0)
+        if (maxHp < 0)
         {
-            this.maxHP = 100f;
+            this.maxHp = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default..");
         }
-        if (hp > maxHP)
-        {
-            this.hp = maxHP;
-        }
+
         this.name = name;
-        this.maxHP = maxHP;
-        this.hp = maxHP;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
     }
 
     ///<summary> method that prints health</summary>
     public void PrintHealth()
     {
-        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHP} health");
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
     }
 
 }
