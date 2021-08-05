eccfd1d6a3dc4ad627631325e820adc0d69ec7ed
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-05 20:45:27 +0000

    Create a public class Player

--- 0x03-csharp-delegates_events/0-universal_health/Program.cs
+++ 0x03-csharp-delegates_events/0-universal_health/Program.cs
@@ -11,10 +11,11 @@ class Player
     ///<summary> constructor for the player stats</summary>
     public Player(string name = "Player", float maxHp = 100f)
     {
-        if (maxHp < 0)
+        if (maxHp < 0f)
         {
             this.maxHp = 100f;
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default..");
+            this.hp = maxHp;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
 
         this.name = name;
